7c6e240cc18d59c17d38260fd254e1fee67d21e7
Chirag Patel <chiragpatel499@gmail.com>
2025-11-14 16:03:11 +0530

    ok

--- marine/marine/settings.py
+++ marine/marine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h=5%hrqy21p5yoq2czffg+6m5qi*w(zw(h54z+3sn&32(%(1kn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
