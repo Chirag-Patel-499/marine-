0464203c8e344b543fe4287578a8f691f0f53ceb
Chirag Patel <chiragpatel499@gmail.com>
2025-11-19 10:29:46 +0530

    staticfiles

--- marine/marine/settings.py
+++ marine/marine/settings.py
@@ -84,7 +84,7 @@ USE_TZ = True
 # ⬇️ STATIC & MEDIA (LIVE SERVER IMPORTANT PART)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'   # collectstatic folder
